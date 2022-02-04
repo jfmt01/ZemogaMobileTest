7a41ea53e7569cc0acdc59beda7d690da3b2d260
Fernando Moreno <jeison.jfmt@gmail.com>
2022-02-03 19:22:43 -0500

    Initial Commit

---
+++ ZemogaMobileTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZemogaMobileTest
+//
+//  Created by Fernando  Moreno on 3/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
