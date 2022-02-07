4034db7f71a73fff80c9da970d21988326c0705a
Fernando Moreno <jeison.jfmt@gmail.com>
2022-02-07 09:41:18 -0500

    DataBase manager setup

--- ZemogaMobileTest/ZemogaMobileTest/Config/DataBase/DBManager.swift
+++ ZemogaMobileTest/ZemogaMobileTest/Config/DataBase/DBManager.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+import RealmSwift
+
+class DBManager{
+    //Mar: - Properties
+    static let shared = DBManager()
+    let realm = try! Realm()
+    
+    private init(){}
+    
+    
+    func addPostDB(posts:[RealmPost]){
+        do{
+            try realm.write{
+                realm.add(posts)
+            }
+        }catch let error as NSError{
+            print("Realm ERROR - Adding post: \(error)")
+        }
+    }
+    
+    func checkPostAsReadDB(post: RealmPost){
+        do {
+            try realm.write {
+                post.wasRead = true
+            }
+        } catch let error as NSError {
+            print("Realm ERROR - Marking post as read: \(error)")
+        }
+    }
+    
+    func deletePostDB(post:[RealmPost]){
+        do{
+            try realm.write{
+                realm.delete(post)
+            }
+        }catch let error as NSError{
+            print("Realm ERROR - Delete individual post: \(error)")
+        }
+    }
+    
+    func deleteAllDB(posts:[RealmPost]){
+        do{
+            try realm.write{
+                realm.delete(posts)
+            }
+        }catch let error as NSError{
+            print("Realm ERROR - Deleting all: \(error)")
+        }
+    }
+    
+    func fetchPostsDB() -> [RealmPost]{
+        do{
+            let realm = try Realm()
+            return Array(realm.objects(RealmPost.self))
+        }catch let error as NSError{
+            print("Realm ERROR - Updating a post: \(error)")
+        }
+        
+        return[RealmPost]()
+    }
+    
+    func addPostToFavoritesDB(post: RealmPost, isFavorite: Bool){
+        do {
+            try realm.write {
+                post.isFavorite = isFavorite
+            }
+        } catch let error as NSError {
+            print("Realm ERROR - Adding post to favorites: \(error)")
+        }
+    }
+}
