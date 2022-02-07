2b7d66232859af08733f4dbbc18997c3261d2ff2
Fernando Moreno <jeison.jfmt@gmail.com>
2022-02-07 07:16:04 -0500

    Posts and comments cells graphic adjustment

---
+++ ZemogaMobileTest/ZemogaMobileTest/Views/Cells/CommentsList/CommentsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentsTableViewCell.swift
+//  ZemogaMobileTest
+//
+//  Created by Fernando  Moreno on 7/02/22.
+//
+
+import UIKit
+
+class CommentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
